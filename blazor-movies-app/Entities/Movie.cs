28e6051114e407ddbdaad3e4b5602304e454234d
Unai Villar <unaidevcoding@gmail.com>
2026-05-07 19:48:17 +0200

    Add project files.

---
+++ blazor-movies-app/Entities/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace blazor_movies_app.Entities
+{
+    public class Movie
+    {
+
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+
+    }
+}
