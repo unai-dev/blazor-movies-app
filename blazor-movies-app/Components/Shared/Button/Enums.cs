59d04f25da184f2a75e15b197bad4734b24d798a
Unai <unaidevcoding@gmail.com>
2026-05-08 13:39:11 +0200

    finish 39 section

---
+++ blazor-movies-app/Components/Shared/Button/Enums.cs
@@ -0,0 +1,9 @@
+﻿namespace blazor_movies_app.Components.Shared.Button
+{
+    public enum ButtonType
+    {
+        Button,
+        Sumbit,
+        Reset
+    }
+}
---
+++ blazor-movies-app/Components/Shared/Enums.cs
@@ -0,0 +1,19 @@
+﻿namespace blazor_movies_app.Components.Shared
+{
+    public enum Variante
+    {
+        Primary,
+        Secondary,
+        Exit,
+        Warning,
+        Error
+
+    }
+
+    public enum Size
+    {
+        Sm,
+        Md,
+        Lg
+    }
+}
