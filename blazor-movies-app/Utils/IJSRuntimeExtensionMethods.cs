a05ab7d49116ecbc80f7be8800fd937e936892a6
Unai <unaidevcoding@gmail.com>
2026-05-11 14:16:19 +0200

    add alert confirm to delete movie

---
+++ blazor-movies-app/Utils/IJSRuntimeExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿using blazor_movies_app.Entities;
+using Microsoft.JSInterop;
+
+namespace blazor_movies_app.Utils
+{
+    public static class IJSRuntimeExtensionMethods
+    {
+        public async static ValueTask<bool> Confirm(this IJSRuntime jSRuntime, string message)
+        {
+            return await jSRuntime.InvokeAsync<bool>("confirm", message);
+        }
+    }
+}
