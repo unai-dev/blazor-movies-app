8681dca10727ca0aeb635c0d405469fb5184f49f
Unai <unaidevcoding@gmail.com>
2026-05-17 10:39:17 +0200

    add search movie page

---
+++ blazor-movies-app/Entities/Author.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace blazor_movies_app.Entities
+{
+    public class Author
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public string? Name { get; set; }
+        public string? ImageURL { get; set; }
+        public DateTime? Birhdate { get; set; }
+    }
+}
